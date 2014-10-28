49866c9b62ae50afaa5ec3ddaa7cb30100aa6598
yitznewton <yitznewton@hotmail.com>
2014-10-28 01:36:45 -0400

    title case renderer

---
+++ src/Yitznewton/Procslyte/Render/TextCase/TitleCaseRenderer.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace Yitznewton\Procslyte\Render\TextCase;
+
+use Yitznewton\Procslyte\Render\Renderer;
+
+class TitleCaseRenderer implements Renderer
+{
+    private static $stopWords = [
+        'a',
+        'an',
+        'and',
+        'as',
+        'at',
+        'but',
+        'by',
+        'down',
+        'for',
+        'from',
+        'in',
+        'into',
+        'nor',
+        'of',
+        'on',
+        'onto',
+        'or',
+        'over',
+        'so',
+        'the',
+        'till',
+        'to',
+        'up',
+        'via',
+        'with',
+        'yet',
+    ];
+
+    private $internalRenderer;
+
+    /**
+     * @param array $settings
+     * @param Renderer $internalRenderer
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function __construct(array $settings, Renderer $internalRenderer)
+    {
+        $this->internalRenderer = $internalRenderer;
+    }
+
+    /**
+     * @param array $citationData
+     * @return string
+     */
+    public function render(array $citationData)
+    {
+        $internalString = $this->internalRenderer->render($citationData);
+
+        if ($this->isAllUpper($internalString)) {
+            return $this->renderUpper($internalString);
+        } else {
+            return $this->renderMixed($internalString);
+        }
+    }
+
+    private function renderUpper($string)
+    {
+        return implode(' ', $this->renderWords($string, 'renderUpperWord'));
+    }
+
+    private function renderMixed($string)
+    {
+        return implode(' ', $this->renderWords($string, 'renderMixedWord'));
+    }
+
+    /**
+     * @param string $string
+     * @param string $method
+     * @return array
+     */
+    private function renderWords($string, $method)
+    {
+        return array_map([$this, $method], $this->words($string));
+    }
+
+    /**
+     * @SuppressWarnings(PHPMD.UnusedPrivateMethod)
+     */
+    private function renderUpperWord($word)
+    {
+        $lowerWord = strtolower($word);
+
+        if (in_array($lowerWord, self::$stopWords)) {
+            return $lowerWord;
+        }
+
+        return ucfirst($lowerWord);
+    }
+
+    /**
+     * @SuppressWarnings(PHPMD.UnusedPrivateMethod)
+     */
+    private function renderMixedWord($word)
+    {
+        if (!$this->isAllLower($word)) {
+            return $word;
+        }
+
+        $lowerWord = strtolower($word);
+
+        if (in_array($lowerWord, self::$stopWords)) {
+            return $lowerWord;
+        }
+
+        return ucfirst($lowerWord);
+    }
+
+    /**
+     * @param $string
+     * @return array
+     */
+    private function words($string)
+    {
+        return explode(' ', $string);
+    }
+
+    private function isAllUpper($string)
+    {
+        return !preg_match('/[a-z]/', $string);
+    }
+
+    private function isAllLower($string)
+    {
+        return !preg_match('/[A-Z]/', $string);
+    }
+}
--- tests/Yitznewton/Tests/Procslyte/Render/CaseRendererTest.php
+++ tests/Yitznewton/Tests/Procslyte/Render/CaseRendererTest.php
@@ -5,6 +5,7 @@ namespace Yitznewton\Tests\Procslyte\Render;
 use Yitznewton\Procslyte\Render\TextCase\CapitalizeAllRenderer;
 use Yitznewton\Procslyte\Render\TextCase\CapitalizeFirstRenderer;
 use Yitznewton\Procslyte\Render\TextCase\LowercaseRenderer;
+use Yitznewton\Procslyte\Render\TextCase\TitleCaseRenderer;
 use Yitznewton\Procslyte\Render\TextCase\UppercaseRenderer;
 
 class CaseRendererTest extends \PHPUnit_Framework_TestCase
@@ -35,5 +36,33 @@ class CaseRendererTest extends \PHPUnit_Framework_TestCase
         $innerRenderer = new StubRenderer('foo the bar baz');
         $renderer = new CapitalizeFirstRenderer([], $innerRenderer);
         $this->assertEquals('Foo the bar baz', $renderer->render([]));
+    }
+
+    public function testTitleCaseWithUpper()
+    {
+        $innerRenderer = new StubRenderer('JIM JAM');
+        $renderer = new TitleCaseRenderer([], $innerRenderer);
+        $this->assertEquals('Jim Jam', $renderer->render([]));
+    }
+
+    public function testTitleCaseWithUpperAndStopWord()
+    {
+        $innerRenderer = new StubRenderer('JIM THE JAM');
+        $renderer = new TitleCaseRenderer([], $innerRenderer);
+        $this->assertEquals('Jim the Jam', $renderer->render([]));
+    }
+
+    public function testTitleCaseWithMixed()
+    {
+        $innerRenderer = new StubRenderer('jIm jAm');
+        $renderer = new TitleCaseRenderer([], $innerRenderer);
+        $this->assertEquals('jIm jAm', $renderer->render([]));
+    }
+
+    public function testTitleCaseWithMixedHavingUpperWords()
+    {
+        $innerRenderer = new StubRenderer('jim ALL jam');
+        $renderer = new TitleCaseRenderer([], $innerRenderer);
+        $this->assertEquals('Jim ALL Jam', $renderer->render([]));
     }
 }
