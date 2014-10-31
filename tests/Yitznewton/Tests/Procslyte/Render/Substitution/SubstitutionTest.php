278a27528347e39ffcebe83cf6138a7ddfb1503e
yitznewton <yitznewton@hotmail.com>
2014-10-31 08:58:09 -0400

    clean substitution tests

--- tests/Yitznewton/Tests/Procslyte/Render/Substitution/SubstitutionTest.php
+++ tests/Yitznewton/Tests/Procslyte/Render/Substitution/SubstitutionTest.php
@@ -7,44 +7,67 @@ use Yitznewton\Procslyte\Render\Text\ValueRenderer;
 
 class SubstitutionTest extends \PHPUnit_Framework_TestCase
 {
-    public function testWithDefaultReturn()
+    private $listener;
+
+    public function setUp()
+    {
+        $this->listener = new SpySubstitutionListener();
+    }
+
+    private function createRenderer($value)
+    {
+        $defaultRenderer = new ValueRenderer($value);
+        return new SubstitutableRenderer($defaultRenderer, $this->listener);
+    }
+
+    /**
+     * @test
+     */
+    public function withDefaultReturn()
     {
         $defaultValue = 'foo';
-        $defaultRenderer = new ValueRenderer($defaultValue);
-        $listener = new SpySubstitutionListener();
-        $renderer = new SubstitutableRenderer($defaultRenderer, $listener);
-        $this->assertEquals($defaultValue, $renderer->render([]));
-        $this->assertFalse($listener->receivedEvent());
+        $renderer = $this->createRenderer($defaultValue);
+
+        $renderedResult = $renderer->render([]);
+
+        $this->assertEquals($defaultValue, $renderedResult);
+        $this->assertFalse($this->listener->receivedEvent());
     }
 
-    public function testWithAllEmpty()
+    /**
+     * @test
+     */
+    public function withAllEmpty()
     {
-        $defaultRenderer = new ValueRenderer('');
-        $listener = new SpySubstitutionListener();
-        $renderer = new SubstitutableRenderer($defaultRenderer, $listener);
-        $renderer->addSubstitute('blank', $defaultRenderer);
-        $this->assertSame('', $renderer->render([]));
-        $this->assertFalse($listener->receivedEvent());
+        $renderer = $this->createRenderer('');
+        $blankRenderer = new ValueRenderer('');
+        $renderer->addSubstitute('blank', $blankRenderer);
+
+        $renderedResult = $renderer->render([]);
+
+        $this->assertSame('', $renderedResult);
+        $this->assertFalse($this->listener->receivedEvent());
     }
 
-    public function testWithSecondarySubstituteReturn()
+    /**
+     * @test
+     */
+    public function withSecondarySubstituteReturn()
     {
-        $defaultRenderer = new ValueRenderer('');
-
-        $listener = new SpySubstitutionListener();
-        $renderer = new SubstitutableRenderer($defaultRenderer, $listener);
+        $renderer = $this->createRenderer('');
 
         $blankRenderer = new ValueRenderer('');
+        $renderer->addSubstitute('blank', $blankRenderer);
 
         $substituteName = 'title';
         $substituteValue = 'bar';
         $titleRenderer = new ValueRenderer($substituteValue);
-
-        $renderer->addSubstitute('blank', $blankRenderer);
         $renderer->addSubstitute($substituteName, $titleRenderer);
 
-        $this->assertEquals($substituteValue, $renderer->render([]));
-        $this->assertTrue($listener->receivedEvent());
-        $this->assertEquals($substituteName, $listener->getEvent()->getNameOfSubstitute());
+        $renderedResult = $renderer->render([]);
+
+        $this->assertEquals($substituteValue, $renderedResult);
+        $this->assertTrue($this->listener->receivedEvent());
+        $this->assertEquals($substituteName, $this->listener->getEvent()->getNameOfSubstitute());
     }
 }
